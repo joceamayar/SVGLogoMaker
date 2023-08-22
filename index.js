7a97644789aea785a2662cc47c0bd5678e91feca
joceamayar <jocelin@tavlee.com>
2023-08-21 23:31:36 -0400

    updated triangle text to fit in triangle, updated parameters in the classes

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 
 const fs = require("fs")
 
-//brackets before the equal sign means that I'm deconstructing. 
+//we are using require to get the funtinability from SVG.js file.
 const SVG = require("./lib/SVG.js")
+
+//we are creating an instance of the SVG class 
 let svgShapes = new SVG("AND", "green", "Triangle", "yellow");
 console.log(svgShapes);
+
 let svgString = `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
     ${svgShapes.render()}
-    <text x="150" y="125" font-size="60" text-anchor="middle" fill="white">SVG</text>
+    
     </svg>`;
 
 fs.writeFile("lib/shape.svg", svgString, function (err) {
